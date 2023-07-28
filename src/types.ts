21e12dcd9a03d65f5881249070f5b338950fc3d1
Teker <105823858+teker821@users.noreply.github.com>
2023-07-27 18:42:31 -0600

    first commit

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export type Vector = {
+    Point: Number;
+}
+
+export type Vector2 = {
+    X: Vector;
+    Y: Vector;
+}
+
+export type Vector3 = {
+    X: Vector;
+    Y: Vector;
+    Z: Vector;
+}
